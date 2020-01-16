342b187ed1cc91bd35b02d43c32ff4a41290c6bb
JingfeiPeng <pengjefferbms@gmail.com>
2020-01-16 11:29:38 -0500

    restored to previous state

--- front-end/src/app/suduku/suduku.component.ts
+++ front-end/src/app/suduku/suduku.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../services/api-call.service';
 
 @Component({
   selector: 'app-suduku',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suduku.component.scss']
 })
 export class SudukuComponent implements OnInit {
-
-  constructor() { }
+  initialSudoku: string[][] = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]];
+  constructor(private apiCall : ApiCallService) { }
 
   ngOnInit() {
+    this.apiCall.solver(this.initialSudoku)
+    .subscribe(res => console.log(res))
   }
 
 }
